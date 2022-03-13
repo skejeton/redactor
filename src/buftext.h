e56d61934e4bb21cf498a25f0aea5b11bddc8259
ishidex2 <artsbyduo@gmail.com>
2022-03-13 19:27:28 +0300

    Cursors yay

--- src/buftext.h
+++ src/buftext.h
@@ -38,6 +38,32 @@ static SDL_Point get_buffer_text_pass_starting_position(struct buftext_pass *pas
     return (SDL_Point){position.x + pass->scroll.x, position.y + pass->scroll.y};
 }
 
+// Returns the rectangle size of the text that's right before the `marker`
+// It considers the scroll and position
+//------------------------------------------------------------------------
+// x - The X position of the line
+// y - The Y position of the line
+// w - The width of the text that is before the marker
+// h - The height of the line
+static SDL_Rect get_buffer_text_pretext_rect(struct buftext_pass *pass, struct buffer_marker marker)
+{
+    marker = buffer_sanitize_marker(pass->buffer, marker);
+    char *line = buffer_get_range(pass->buffer, buffer_marker_pretext_range(pass->buffer, marker));
+    SDL_Point size = font_measure_text(pass->font, line);
+    int y_offset = size.y * marker.line;
+    SDL_Point position = get_buffer_text_pass_starting_position(pass);
+    free(line);
+
+    return (SDL_Rect) {
+        .x = position.x,
+        .y = position.y+y_offset,
+        .w = size.x,
+        .h = size.y
+    };
+}
+
+
+
 static void draw_buffer_text(struct buftext_pass *pass)
 {
     SDL_Point line_size, position = get_buffer_text_pass_starting_position(pass);
--- src/docview.c
+++ src/docview.c
@@ -4,30 +4,44 @@
 static void draw_number_line(struct buftext_pass *pass)
 {
     apply_buffer_text_pass(pass);
-    struct buffer *buffer = pass->buffer;
     SDL_Renderer *renderer = pass->renderer;
     struct font *font = pass->font;
     SDL_Point position = get_buffer_text_pass_starting_position(pass), line_size;
-    const int number_line_padding = font_get_height(font);
-    char line_number_text_buffer[32];
-    int maximum_line_width = 0;
+    const int right_padding = font_get_height(font);
+    char number_buf[32];
+    int max_width = 0;
 
-    for (int i = 0; i < buffer->line_count; ++i) {
+    for (int i = 0; i < pass->buffer->line_count; ++i) {
         TODO("Replace snprintf with a faster int-to-string converter");
-        snprintf(line_number_text_buffer, sizeof line_number_text_buffer, "%d", i+1);
-        line_size = font_write_text(font, line_number_text_buffer, position, renderer);
+        snprintf(number_buf, sizeof number_buf, "%d", i+1);
+        line_size = font_write_text(font, number_buf, position, renderer);
         position.y += line_size.y;
-        if (maximum_line_width < line_size.x)
-            maximum_line_width = line_size.x;
+        if (max_width < line_size.x)
+            max_width = line_size.x;
     }
 
-    rect_cut_left(&pass->viewport, maximum_line_width + number_line_padding);
+    rect_cut_left(&pass->viewport, max_width + right_padding);
 }
 
+static SDL_Rect convert_pretext_rect_to_cursor(SDL_Rect rect)
+{
+    rect.x += rect.w;
+    rect.w = 2;
+    return rect;
+}
 
+static void draw_cursor(struct buftext_pass *pass, SDL_Color color, struct buffer_marker marker)
+{
+    SDL_Rect cursor_rect = convert_pretext_rect_to_cursor(get_buffer_text_pretext_rect(pass, marker));
+
+    set_renderer_color_from_sdl_color(pass->renderer, color);
+    SDL_RenderFillRect(pass->renderer, &cursor_rect);
+}
 
 void dv_draw(struct docview *view, SDL_Renderer *renderer)
 {
+    const SDL_Color primary_cursor_color = {220, 150, 0, 255};
+    const SDL_Color secondary_cursor_color = {0, 150, 220, 255};
     struct buftext_pass text_pass = {
         .buffer = &view->document.buffer,
         .font = view->font,
@@ -41,6 +55,8 @@ void dv_draw(struct docview *view, SDL_Renderer *renderer)
     draw_number_line(&text_pass);
     draw_buffer_text(&text_pass);
     end_buffer_pass(&text_pass);
+    draw_cursor(&text_pass, secondary_cursor_color, view->document.cursor.selection.from);
+    draw_cursor(&text_pass, primary_cursor_color, view->document.cursor.selection.to);
 }
 
 void dv_scroll(struct docview *view, float dx, float dy)
