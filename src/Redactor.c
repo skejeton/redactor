bc9a0c84f5bf7dba5d5fc82e2a386b0d3b7638a8
ishidex2 <artsbyduo@gmail.com>
2022-03-29 20:28:02 +0300

    Show file to console

--- src/Redactor.c
+++ src/Redactor.c
@@ -1,17 +1,48 @@
+#include <string.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#define DieErr(...) do {fprintf(stderr, __VA_ARGS__); exit(-1);} while (0)
 
 struct {
-        const char *message;
+        const char   *file_name;
+        FILE         *file_handle;
+        char         *file_data;
 } 
 typedef Redactor;
 
-void Redactor_PrintMessage(Redactor *rs)
+char* Util_ReadFileStr(FILE *f)
 {
-        printf("%s", rs->message);
+        char *s;
+        fseek(f, 0, SEEK_END);
+        size_t size = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        s = malloc(size + 1);
+        s[fread(s, 1, size, f)] = 0;
+        return s;
+}
+
+// Checks out the arguments and sets needed values
+int Redactor_UseArgs(Redactor *rs, int argc, char *argv[])
+{
+        if (argc != 2) {
+                DieErr("Usage: %s file.txt\n", argv[0]);
+        }
+
+        rs->file_name = argv[1];
+        rs->file_handle = fopen(rs->file_name, "rw");
+        if (!rs->file_handle) {
+                DieErr("Fatal: Error opening file %s: %s", rs->file_name, strerror(errno));
+        }
+        rs->file_data = Util_ReadFileStr(rs->file_handle);
+        
 }
 
 int Redactor_Main(int argc, char *argv[])
 {
-        Redactor rs = { "Redactor main\n" };
-        Redactor_PrintMessage(&rs);
+        Redactor rs = {0};
+        Redactor_UseArgs(&rs, argc, argv);
+        printf("%s", rs.file_data);
+        free(rs.file_data);
 }
