ca7296c002d0932ea740d2a1f42df6332607825c
ishidex2 <ishidex2@gmail.com>
2022-07-06 20:27:48 +0300

    Port build system from cave tropes

--- Test.mk
+++ Test.mk
@@ -1,24 +1,19 @@
 CC?=clang
 AR?=llvm-ar
-OUTPUT?=bin/redactor
+OUTPUT?=bin/test
 SYS_LIBS?=-lSDL2 -lSDL2_ttf -lm -lc
 
 SHARED_CFLAGS=-O0 -g -fsanitize=address
 
 SRC_CFLAGS=-I. -Isrc -MD
 SRC_LIBS=$(SYS_LIBS) 
-SRC_UNITS=$(shell find src/ -type f -name "*.c")
-SRC_HEADERS=$(shell find src/ -type f -name "**.h" -o "src/**.inl")
+SRC_UNITS=$(shell find src/ -type f -name "*.c" -not -name "Main.c") $(shell find tests/ -type f -name "*.c")
+SRC_HEADERS=$(shell find src/ -type f -name "**.h" -o "**.inl") $(shell find tests/ -type f -name "**.h" -o "**.inl")
 SRC_OBJECTS=$(SRC_UNITS:.c=.o)
 SRC_DEPS=$(SRC_OBJECTS:.o=.d)
 
 all: $(OUTPUT)
-
-run: all
-	$(OUTPUT) tests/TestInputUnicodeBasic.txt
-
-test:     
-	@make -f Test.mk
+	$(OUTPUT) $(args)
 
 %.o: %.c
 	$(CC) $(SHARED_CFLAGS) $(SRC_CFLAGS) -c $< -o $@
@@ -30,7 +25,5 @@ clean:
 	rm -f $(OUTPUT)
 	rm -f $(SRC_OBJECTS)
 	rm -f $(SRC_DEPS)
-	make -f Test.mk clean
 
 include $(wildcard $(SRC_DEPS))
-.PHONY: all run test clean
